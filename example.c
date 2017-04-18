cedeaa04133e7321b090767411d8051633b0edc5
Léonard Marques <lmarques@student.42.fr>
2017-04-18 23:21:14 +0200

    added comments

--- example.c
+++ example.c
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 03:51:42 by lmarques          #+#    #+#             */
-/*   Updated: 2017/04/18 04:13:28 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/04/18 23:20:52 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,54 @@
 
 int	main(void)
 {
-	t_mlx	mlx;
+	t_mlx	mlx; //Here I first create my struct that will contains all the "MLX stuff"
 	int		count_w;
 	int		count_h;
 
 	count_h = -1;
-	mlx.mlx_ptr = mlx_init();
+	mlx.mlx_ptr = mlx_init(); //First you need to call mlx_init and store its return value.
+	//Now do the same with mlx_new_window
 	mlx.win = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "A simple example");
+	//One more time with mlx_new_image
 	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
+	/*
+	 Now the important part :
+	 mlx_get_data_addr will return a char * that is 3 time the width * height of your imqge.
+	 Why so ? Let me explain : This char * will represent your image, pixel by pixel,
+	 and the values of this array are the colors. That's why the array is 3 times bigger :
+	 you need 3 char to code the color of each pixels (one for Red, Green and Blue).
+	 But... it's very convenient, right ? So here is my little trick : you cast
+	 mlx_get_data_addr as an int * and store it in an int *.
+	 This wqy, the array will have the exact same size as your window, and each index
+	 will represent one complete color of a pixel !
+	*/
 	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l,
 		&mlx.img.endian);
+	/*
+	 Now just a little example : here a loop that will draw each pixels that
+	 have an odd width with and the ones that have an even width black.
+	*/
 	while (++count_h < WIN_HEIGHT)
 	{
 		count_w = -1;
 		while (++count_w < WIN_WIDTH)
 		{
 			if (count_w % 2)
+				/*
+				 As you can see here instead of using the mlx_put_pixel function
+				 I just assign a color to each pixel one by one in the image
+				 and the image will be printed in one time at the end of the loop.
+				 Now one thing to understand here is that you're workinh on a 1-dimensional
+				 array, while your window is (obviously) 2-dimensional.
+				 So, instead of having data[height][width] here you'll have the following
+				 fornula : current height * max width + current width (as you can see below)
+				*/
 				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
 			else
 				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0;
 		}
 	}
+	//Now you just have to print the image using mlx_put_image_to_window !
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
--- example.h
+++ example.h
@@ -6,7 +6,7 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/25 01:19:41 by lmarques          #+#    #+#             */
-/*   Updated: 2017/04/18 04:07:15 by lmarques         ###   ########.fr       */
+/*   Updated: 2017/04/18 22:47:38 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,38 @@
 # include "./libft/libft.h"
 # include "./libft/get_next_line.h"
 
+/*
+ Defines for the with and height of your window. I suggest you to do the same so
+ you can change easily the size of your window later if needed.
+*/
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 
+/*
+ Here I built a struct of the MLX image :
+ It contains everything you need.
+ - img_ptr to store the return value of mlx_new_image
+ - data to store the return value of mlx_get_data_addr
+ - the 3 other variables are pretty nuch useless, but you'll need
+ them in the prototype of mlx_get_data_addr (see the man page for that)
+ */
 typedef struct	s_img
 {
 	void		*img_ptr;
-	int			*data;
+	int			*data; //Here you got an int * while mlx_get_data_addr returns
+						//a char *, i'll talk more about it in the .c file.
+//Here are the 3 "useless" variables.
 	int			size_l;
 	int			bpp;
 	int			endian;
 }				t_img;
 
+/*
+ Here is my main struct cantaininh every variables needed by the MLX.
+ - mlx_ptr stores the return value of mlx_init
+ - win stores the return value of mlx_new_window
+ - img will store everything we need for the image part, the struct is described above.
+ */
 typedef struct	s_mlx
 {
 	void		*mlx_ptr;
