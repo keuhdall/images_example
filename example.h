821557790620d9d28a6af6c7f227b0f71f5503f3
Léonard Marques <lmarques@student.42.fr>
2017-09-27 11:30:24 +0200

    Corrected typos

--- example.h
+++ example.h
@@ -30,15 +30,15 @@
  It contains everything you need.
  - img_ptr to store the return value of mlx_new_image
  - data to store the return value of mlx_get_data_addr
- - the 3 other variables are pretty nuch useless, but you'll need
+ - the 3 other variables are pretty much useless, but you'll need
  them in the prototype of mlx_get_data_addr (see the man page for that)
  */
 typedef struct	s_img
 {
 	void		*img_ptr;
-	int			*data; //Here you got an int * while mlx_get_data_addr returns
-						//a char *, i'll talk more about it in the .c file.
-//Here are the 3 "useless" variables.
+	int			*data; //Here you got an int * even though mlx_get_data_addr returns
+						//a char *, i'll talk more about this in the .c file.
+//Here are the 3 "useless" variables. You can find more informations about these in the man page.
 	int			size_l;
 	int			bpp;
 	int			endian;
